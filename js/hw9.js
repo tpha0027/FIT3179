88997e44484523cc1715a12f5b938312c0a3b105
tpha0027 <75285096+tpha0027@users.noreply.github.com>
2022-09-24 10:22:58 +1000

    Delete js directory

--- js/hw9.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "hw9.vg.json";
-vegaEmbed("#map_visua", vg_1).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
