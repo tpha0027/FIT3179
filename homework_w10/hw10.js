0d725423c8aaf1a41bdd3e620af07715b8afe47f
tpha0027 <75285096+tpha0027@users.noreply.github.com>
2022-10-08 17:12:21 +1100

    Add files via upload

---
+++ homework_w10/hw10.js
@@ -0,0 +1,4 @@
+var vg_2 = "hw10.vg.json";
+vegaEmbed("#w10_visua", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
