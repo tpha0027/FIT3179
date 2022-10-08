bf15ccb47d969b693f2b70443f60aa37c7a1d332
tpha0027 <75285096+tpha0027@users.noreply.github.com>
2022-10-08 17:06:26 +1100

    Update hw10.js

--- homework_w10/hw10.js
+++ homework_w10/hw10.js
@@ -1,4 +1,4 @@
-var vg_1 = "hw10.vg.json";
-vegaEmbed("#map_visua", vg_1).then(function(result) {
+var vg_2 = "hw10.vg.json";
+vegaEmbed("#map_visua", vg_2).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
