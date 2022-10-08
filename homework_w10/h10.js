d5d7642dfed0fe964159737484ee29a17c38002d
tpha0027 <75285096+tpha0027@users.noreply.github.com>
2022-10-08 17:03:31 +1100

    Delete h10.js

--- homework_w10/h10.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "hw10.vg.json";
-vegaEmbed("#map_visua", vg_1).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
