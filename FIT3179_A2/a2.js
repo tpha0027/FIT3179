ff005ca1eadc7a01bc8d5bbaf81e028018a31572
tpha0027 <75285096+tpha0027@users.noreply.github.com>
2022-10-15 23:28:23 +1100

    Add files via upload

---
+++ FIT3179_A2/a2.js
@@ -0,0 +1,29 @@
+var vg_map = "geomap.vg.json";
+vegaEmbed("#geo_map", vg_map).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_trend = "yearly_trend.vg.json";
+vegaEmbed("#year_trend", vg_trend).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_pie = "pie.vg.json";
+vegaEmbed("#pie", vg_pie).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_circle = "rating.vg.json";
+vegaEmbed("#cir", vg_circle).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_bar = "bar.vg.json";
+vegaEmbed("#bar", vg_bar).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_ge = "genre.vg.json";
+vegaEmbed("#ge", vg_ge).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
